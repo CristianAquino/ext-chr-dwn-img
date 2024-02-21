7092e0eb99804e9105e7bffe55a9ea4ca9fe92f0
cristianaquino <ycristian10@gmail.com>
2024-02-20 21:05:33 -0500

    feat: update

--- content.js
+++ content.js
@@ -1,35 +1,36 @@
 // {document} - currentSrc
 
 async function run() {
-  // let se = JSON.parse(sessionStorage.getItem("data"));
-
-  // console.log("grettings from content");
-  // console.log(se);
-
-  // if (se) {
-  //   chrome.storage.local.set({ data: se });
-  // } else {
   const urls = [];
   const exts = ["jpg", "jpeg", "png", "gif", "avif", "webp"];
-
+  // iteramos el document.images
   for (const iterator of window.document.images) {
+    // validamos su extension
+    const rgx = /^(http|https)/gi;
+    // obtenemos el ultimo string
     let ext = iterator.currentSrc.split(".").pop();
-    if (exts.includes(ext)) {
-      urls.push(iterator.currentSrc);
+    // verificamos su valor
+    if (exts.includes(ext) || rgx.test(iterator.currentSrc)) {
+      if (!urls.includes(iterator.currentSrc)) {
+        urls.push(iterator.currentSrc);
+      }
     }
   }
-  const msg = dataMessage(urls);
 
-  sessionStorage.setItem("data", JSON.stringify(msg));
+  const msg = dataMessage(urls);
+  // realizamos el almacenamiento de datos
+  // al session storage y
+  // al local storage de chrome
+  // sessionStorage.setItem("data", JSON.stringify(msg));
   await chrome.storage.local.set({ data: msg });
 }
-
+// funcion de retorno en base al
+// contenido de el array
 function dataMessage(urls) {
   if (urls.length === 0) {
     return "not found image in current page";
   }
   return urls;
-  // }
 }
 
 run();
--- popup.js
+++ popup.js
@@ -1,10 +1,13 @@
 const btnInit = document.getElementById("btn_image");
+const btnDownload = document.getElementById("btn_download");
 const message = document.getElementById("msg");
 const sec = document.getElementById("section");
+const downloads = [];
 
 async function inject() {
   try {
-    // inject content in current tab
+    // injectamos el script de content en la
+    // pestaña actual
     const tab = await chrome.tabs.query({ active: true, currentWindow: true });
     if (!tab) return;
     const tabData = tab[0];
@@ -12,7 +15,7 @@ async function inject() {
     if (tabData.status === "complete" && /^(http|https)/gi.test(tabData.url)) {
       await chrome.scripting.executeScript({
         target: { tabId: tabData.id },
-        // file that will be inject
+        // archivo que sera injectado
         files: ["content.js"],
       });
       console.log("inject to content");
@@ -23,7 +26,7 @@ async function inject() {
     console.error("error in load popup");
   }
 }
-
+// agregamos las img encontradas
 function addList(msg) {
   const fig = msg.map(
     (e) => `
@@ -41,25 +44,17 @@ function addList(msg) {
         </figure>
   `
   );
-  fig.unshift(`<h3 id="msg_found">all image found</h3>`);
+  fig.unshift(`<h3 id="msg_found">${msg.length} image found</h3>`);
   fig.push(`<button id="btn_download">download all</button>`);
   sec.innerHTML = fig.join("");
 }
 
 btnInit.addEventListener("click", async () => {
-  inject();
+  // esperamos a que se inject el script de content
+  await inject();
+  // obtenemos la data del storage
   const ja = await chrome.storage.local.get("data");
-  const pa = await chrome.storage.onChanged.addListener(
-    (changes, namespace) => {
-      for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
-        console.log(
-          `Storage key "${key}" in namespace "${namespace}" changed.`,
-          `Old value was "${oldValue}", new value is "${newValue}".`
-        );
-      }
-    }
-  );
-  console.log(pa);
+  // verificamos el tipo de dato que nos devuelve el storage
   if (typeof ja.data === "string") {
     btnInit.style.display = "none";
     sec.style.display = "none";
@@ -67,6 +62,15 @@ btnInit.addEventListener("click", async () => {
   } else {
     btnInit.style.display = "none";
     message.style.display = "none";
-    const list = addList(ja.data);
+    addList(ja.data);
   }
+});
+// evento para seleccionar varios imgs
+sec.addEventListener("click", (e) => {
+  // propagacion de evento en section
+  // para cada img
+  if (e.target && e.target.tagName === "IMG") {
+    downloads.push({ url: e.target.src });
+  }
+  console.log(downloads);
 });
